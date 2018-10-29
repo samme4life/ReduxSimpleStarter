d3a61341ffeef3021be82858649c047cbe15d081
Samme <samme4life@gmail.com>
2018-10-30 05:31:00 +1000

    Sec 2 Lec 31 Video Selection

--- src/index.js
+++ src/index.js
@@ -14,10 +14,15 @@ class App extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: []};
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key: API_KEY, term: 'got'},  (videos) => {
-            this.setState({videos});
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]});
             //this.setState({videos : videos});
         });
     }
@@ -26,8 +31,10 @@ class App extends Component {
         return (
             <div>
                 <SearchBar/>
-                <VideoDetails video={this.state.videos[0]}/>
-                <VideoList videos={this.state.videos}/>
+                <VideoDetails video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos}/>
             </div>);
     }
 }
